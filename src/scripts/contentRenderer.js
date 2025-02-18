58b14800bcf6f61ade4c34021e9384300d2d42fb
miryarik <85996187+miryarik@users.noreply.github.com>
2025-02-18 23:59:07 +0530

    minor adjustment

--- src/scripts/contentRenderer.js
+++ src/scripts/contentRenderer.js
@@ -1,8 +1,79 @@
 import { taskHandler } from "./tasks.js";
 import { projectHandler } from "./projects.js";
-import { getCards, getBullets } from "./utils.js";
+import { getCards, dayFromDate, formatDate } from "./utils.js";
 
 const content = document.querySelector("container content");
+
+function getBullets(tasks) {
+    const bullets = [];
+    
+    tasks.forEach((task) => {
+        const bullet = document.createElement("li");
+        bullet.setAttribute("id", task.id);
+        bullet.setAttribute("class", "task-bullet");
+
+        const name = document.createElement("h3");
+        name.innerText = task.name;
+
+        const description = document.createElement("p");
+        description.innerText = task.description;
+
+        const optionsDiv = document.createElement("div");
+        optionsDiv.setAttribute("class", "options");
+        const deleteButton = document.createElement("button");
+        optionsDiv.appendChild(deleteButton);
+        deleteButton.innerText = "Delete";
+
+        deleteButton.addEventListener("click", () => {
+            taskHandler.deleteTask(task.id);
+            sidebarRenderer.renderTaskList();
+            contentRenderer.renderAllTasks();
+        });
+
+        const infoDiv = document.createElement("div");
+        infoDiv.setAttribute("class", "info");
+        const date = document.createElement("p");
+        const priority = document.createElement("p");
+
+        const dateText = dayFromDate(formatDate(task.dueDate));
+        date.innerText = dateText;
+        switch (dateText) {
+            case "Today":
+                bullet.classList.add("today");
+                break;
+
+            case "Tomorrow":
+                bullet.classList.add("tomorrow");
+                break;
+        }
+
+        switch (task.priority) {
+            case 3:
+                priority.innerText = "High";
+                break;
+
+            case 2:
+                priority.innerText = "Normal";
+                break;
+
+            case 1:
+                priority.innerText = "Low";
+                break;
+        }
+
+        infoDiv.appendChild(date);
+        infoDiv.appendChild(priority);
+
+        bullet.appendChild(name);
+        bullet.appendChild(description);
+        bullet.appendChild(optionsDiv);
+        bullet.appendChild(infoDiv);
+
+        bullets.push(bullet);
+    });
+
+    return bullets;
+}
 
 function renderAllProjects() {
     // get all projects
@@ -73,8 +144,7 @@ function renderUpcoming() {
     const header = document.querySelector("container header");
     header.innerText = "Coming Tasks";
     
-    const upcomingTasks = taskHandler.getUpcomingTasks();
-    
+    const upcomingTasks = taskHandler.getUpcomingTasks();    
     const taskBullets = getBullets(upcomingTasks);
 
     taskBullets.forEach((bullet) => {
--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -17,77 +17,6 @@ export function getNewId() {
     return id;
 }
 
-export function getBullets(tasks) {
-    const bullets = [];
-    
-    tasks.forEach((task) => {
-        const bullet = document.createElement("li");
-        bullet.setAttribute("id", task.id);
-        bullet.setAttribute("class", "task-bullet");
-
-        const name = document.createElement("h3");
-        name.innerText = task.name;
-
-        const description = document.createElement("p");
-        description.innerText = task.description;
-
-        const optionsDiv = document.createElement("div");
-        optionsDiv.setAttribute("class", "options");
-        const deleteButton = document.createElement("button");
-        optionsDiv.appendChild(deleteButton);
-        deleteButton.innerText = "Delete";
-
-        deleteButton.addEventListener("click", () => {
-            taskHandler.deleteTask(task.id);
-            sidebarRenderer.renderTaskList();
-            contentRenderer.renderAllTasks();
-        });
-
-        const infoDiv = document.createElement("div");
-        infoDiv.setAttribute("class", "info");
-        const date = document.createElement("p");
-        const priority = document.createElement("p");
-
-        const dateText = dayFromDate(formatDate(task.dueDate));
-        date.innerText = dateText;
-        switch (dateText) {
-            case "Today":
-                bullet.classList.add("today");
-                break;
-
-            case "Tomorrow":
-                bullet.classList.add("tomorrow");
-                break;
-        }
-
-        switch (task.priority) {
-            case 3:
-                priority.innerText = "High";
-                break;
-
-            case 2:
-                priority.innerText = "Normal";
-                break;
-
-            case 1:
-                priority.innerText = "Low";
-                break;
-        }
-
-        infoDiv.appendChild(date);
-        infoDiv.appendChild(priority);
-
-        bullet.appendChild(name);
-        bullet.appendChild(description);
-        bullet.appendChild(optionsDiv);
-        bullet.appendChild(infoDiv);
-
-        bullets.push(bullet);
-    });
-
-    return bullets;
-}
-
 export function compareTasksByDate(taskOne, taskTwo) {
     return taskOne.dueDate.localeCompare(taskTwo.dueDate);
 }
@@ -138,10 +67,21 @@ export function getCards(projects) {
         name.innerText = project.name;
 
         const description = document.createElement("p");
+        description.setAttribute("class", "description");
         description.innerText = project.description;
 
+        const options = document.createElement("div");
+        options.setAttribute("class", "options");
+        
         const deleteButton = document.createElement("button");
+        deleteButton.setAttribute("id", "delete-btn");
+        options.appendChild(deleteButton);
         deleteButton.innerText = "Delete";
+
+        const editButton = document.createElement("button");
+        editButton.setAttribute("id", "edit-btn");
+        options.appendChild(editButton);
+        editButton.innerText = "Edit";
 
         deleteButton.addEventListener("click", () => {
             projectHandler.deleteProject(project.id);
@@ -152,7 +92,7 @@ export function getCards(projects) {
 
         card.appendChild(name);
         card.appendChild(description);
-        card.appendChild(deleteButton);
+        card.appendChild(options);
 
         card.addEventListener("click", () => {
             contentRenderer.renderProject(project.id);
